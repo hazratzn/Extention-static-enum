002df0f3eebea0ad42d635ef80a95eb3f4011458
unknown <hazratzn@code.edu.az>
2021-12-07 05:06:35 -0800

    Extention Class

---
+++ Extention/Extention/Helper/Extention.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Extention.Helper
+{
+   public static class Extention
+    {
+        public static int CheckString(this string str, string pattern)
+        {
+           int wordCount = 0;
+
+            foreach (Match m in Regex.Matches(str, pattern))
+            {
+                wordCount++; 
+            }
+            return wordCount;
+            
+            
+
+        }
+    }
+}
---
+++ Extention/Extention/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Extention.Helper;
+
+namespace Extention
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Salam Cavid necesen?";
+            Console.WriteLine(name.CheckString("a"));
+        }
+    }
+}
